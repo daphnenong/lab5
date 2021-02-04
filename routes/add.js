59f934488413c997ddec163a3d6c33bebfd32c45
daphnenong <47928132+daphnenong@users.noreply.github.com>
2021-02-03 20:32:07 -0800

    woof

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,12 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
-	// Your code goes here
- }
+exports.addFriend = function (req, res) {
+	var desc = req.query.description;
+	var name = req.query.name;
+	var newFriend = {
+		"name": name,
+		"description": desc,
+		"imageURL": "https://picsum.photos/400/400"
+	}
+	data.friends.push(newFriend)
+};
